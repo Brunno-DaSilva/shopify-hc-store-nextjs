fe41756a4fe0cd87633847156ab44a67338b754c
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-06-15 15:52:50 -0500

    added products to the page

--- components/ProductList.js
+++ components/ProductList.js
@@ -62,9 +62,9 @@ const DUMMY_DATA_PRODUCTS = [
 export default function Example() {
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl mx-auto p-24 px-4 sm:p-32 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
-          Customers also purchased
+          Products
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
